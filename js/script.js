b51f6e2be93d61ffa389f0c394db29fe2da702e2
AnimationBrewmaster <glen@animationbrewery.com>
2016-08-04 13:35:21 -0700

    adding more base functionality

--- js/script.js
+++ js/script.js
@@ -3,42 +3,71 @@
 */
 var nPlayers = 0; // number of players
 var playerNames = [];
+var playerScores = [];
 
 function initGame () {
 	console.log("initializing game");
-	var players = getPlayerNumber();
-	numberOfPlayers(players);
+	getPlayerNumber();
 	inputPlayerNames(nPlayers);
-	
+	startScoring();
 }
 
 function getPlayerNumber () {
 	console.log("getting number of players");
 	var numberOfPlayers = prompt("Enter Number of Players (1-6):", "0");
-	if (numberOfPlayers < 2 || numberOfPlayers > 6) {
+	if (isNaN(parseInt(numberOfPlayers))) {
+		getPlayerNumber();
+	}
+	else if (numberOfPlayers < 2 || numberOfPlayers > 6) {
 		getPlayerNumber();
 	}
 	else {
-		return numberOfPlayers;
+		nPlayers = numberOfPlayers;
+		//return numberOfPlayers;
 	}
 }
 
-function numberOfPlayers(val) {	
-	console.log("parsing player number");
-	nPlayers = parseInt(val);
-}
 
 function inputPlayerNames(val) {
 	console.log("requesting payers names");
 	for (i = 0; i < val; i++) {
 		var playerName = prompt("Enter Name of Player # " + (i + 1), "Name");
 		playerNames[i] = playerName;
+		playerScores[i] = 0;
 	}
 	console.log(playerNames);
+	console.log(playerScores);
+}
+
+function startScoring () {
+	// who starts
+	// loop scoring
+	// input score
+	// win the round?
+	// move to next player
+	// repeat
+}
+
+
+function updateScore() {
+	
 }
 
 
 
+function checkWinner() {
+	var topScore = 399;
+	for (i = 0; i < playerNames.length; i++) {
+		if (playerScores[i] > topScore) {
+			playerScores[i] = topScore;
+			winner = playerNames[i]; 
+		}
+		else {
+			console.log(playerNames[i] + " did not win");
+		}
+		// check score
+	}
+}
 
 
 
